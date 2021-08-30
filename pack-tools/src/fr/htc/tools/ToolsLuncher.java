baaf0ea4cd51bb9e3bee448077c1d0154992b382
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-08-30 12:05:33 +0200

    create new project + Main Class

---
+++ pack-tools/src/fr/htc/tools/ToolsLuncher.java
@@ -0,0 +1,19 @@
+package fr.htc.tools;
+
+public class ToolsLuncher {
+
+	public static void main(String[] args) {
+		
+		//Affichage du menu
+		
+		
+		//Demander à l'utilisateur d'introduire son choix
+		
+		
+		//Switch en fonction du choix
+		
+		//Traiter chaque cas
+
+	}
+
+}
