64baddac39a823d7d0aee0458dca431430636d73
selhtc21 <sel.htc21@gmail.com>
2021-09-01 02:49:46 +0200

    first commit

---
+++ pack-tools/src/fr/htc/tools/core/Pgcd.java
@@ -0,0 +1,13 @@
+package fr.htc.tools.core;
+
+import fr.htc.tools.types.Operation;
+
+public class Pgcd implements Operation {
+
+	@Override
+	public void process(String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
